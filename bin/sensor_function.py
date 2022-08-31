8d675a23860d64927ba3ca7a5086d6a798ac534b
Zhao Hanhua <mrzhaohanhua@hotmail.com>
2022-09-01 00:07:06 +0800

    update

--- bin/sensor_function.py
+++ bin/sensor_function.py
@@ -1,13 +1,16 @@
 import os
+import time
 import logging
 import serial
 import serial.tools.list_ports
 from main import set_logging
 
-BAUD_RATE=9600
-BYTE_SIZE=8
-PARITY=serial.PARITY_NONE
-STOP_BIT=serial.STOPBITS_ONE
+BAUD_RATE = 9600
+BYTE_SIZE = 8
+PARITY = serial.PARITY_NONE
+STOP_BIT = serial.STOPBITS_ONE
+TIMEOUT = 5
+WRITE_TIMEOUT = 5
 
 
 def get_port_list() -> list:
@@ -25,6 +28,38 @@ def get_system_temperature() -> float:
     return int(os.popen("cat /sys/class/thermal/thermal_zone0/temp").read())/1000
 
 
+def byteslist_to_stringlist(bl: bytes) -> list:
+    number_list = list(bl)
+    string_list = []
+    for n in number_list:
+        str = '0x{:02X}'.format(n)
+        string_list.append(str)
+    return string_list
+
+
+def serial_demo():
+    bytes_list = [0x05, 0x04, 0x10, 0x00, 0x00, 0x01, 0x34, 0xbd]
+    ser = serial.Serial(
+        port='/dev/ttyUSB0',
+        baudrate=BAUD_RATE,
+        bytesize=BYTE_SIZE,
+        parity=PARITY,
+        stopbits=STOP_BIT,
+        timeout=TIMEOUT,
+        write_timeout=WRITE_TIMEOUT)
+    ser.flushInput()
+    ser.flushOutput()
+    ser.write(bytes_list)
+    logging.debug(f"write data to '{ser.port}': {bytes_list}")
+    time.sleep(1)
+    recived_data = ser.read(7)
+    if len(recived_data)==7:
+        logging.debug(f"recived_data: {byteslist_to_stringlist(recived_data)}")
+    else:
+        logging.error(f"recived_data: {byteslist_to_stringlist(recived_data)}")
+    pass
+
+
 if __name__ == "__main__":
     set_logging()
-    logging.debug(f"get_system_temperature() = {get_system_temperature()}")
+    serial_demo()
